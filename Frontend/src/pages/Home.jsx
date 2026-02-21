2ba00dcd4084a6e94d477908dd90f9669542b8d8
NishanSunar <barailinishant908@gmail.com>
2026-02-22 00:40:17 +0545

    home: Our policies

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicies from '../components/OurPolicies'
+
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
         <Hero/>
         <LatestCollection/>
         <BestSeller/>
+        <OurPolicies/>
     </div>
   )
 }
