2d08bc9324762072b96b728236b0098d8b158c67
NishanSunar <barailinishant908@gmail.com>
2026-03-17 23:48:59 +0545

    feat:cart Total amount

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import CartTotal from '../components/CartTotal'
 
 const Cart = () => {
 
@@ -57,6 +58,12 @@ const Cart = () => {
 
 
       })}
+      </div>
+
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-112.5'>
+          <CartTotal/>
+        </div>
       </div>
       
     </div>
