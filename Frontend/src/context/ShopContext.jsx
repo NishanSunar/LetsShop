fee29b9c3945df463251f952366fe2b5e9459c93
NishanSunar <barailinishant908@gmail.com>
2026-03-19 00:05:47 +0545

    feat: place order page

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { ToastContainer, toast } from 'react-toastify';
-
+import {useNavigate} from 'react-router'
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) =>{
@@ -11,6 +11,7 @@ const ShopContextProvider = (props) =>{
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
 
     const addToCart = async (itemId, size) =>{
@@ -81,7 +82,7 @@ const ShopContextProvider = (props) =>{
         cartItems, addToCart,
         getCartCount,
         updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
 
     return (
