626dc342cd9bdd030873c826694cabd0a72f929c
NishanSunar <barailinishant908@gmail.com>
2026-03-19 12:34:24 +0545

    fixcing error

--- Frontend/src/pages/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder.jsx
@@ -10,7 +10,7 @@ const PlaceOrder = () => {
   return (
 
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
-      <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+      <div className='flex flex-col gap-4 w-full sm:max-w-120'>
         <div className='text-xl sm:text-2xl my-3'>
           <Title  text1={'DELIVERY'} text2={'INFORMATION'}/>
         </div>
