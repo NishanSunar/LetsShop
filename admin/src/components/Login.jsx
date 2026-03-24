d0a52c60f5699e216d545be5683ed005166cd955
NishanSunar <barailinishant908@gmail.com>
2026-03-25 00:40:29 +0545

    fixed error

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = ({setToken}) => {
 
     <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
         <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
-        <form onSubmit={onSubmitHandler} action="">
+        <form onSubmit={onSubmitHandler}>
         <div className='mb-3 min-w-72'>
             <p className='text-sm font-medium text-grya-700 mb-2'>Email Address</p>
             <input onChange={(e)=> setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='example@domain.com' required/>
