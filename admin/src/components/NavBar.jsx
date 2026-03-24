7314d0fd536eae3eb6f15e2ac139b35888c3be90
NishanSunar <barailinishant908@gmail.com>
2026-03-24 22:11:41 +0545

    Designing Navbar

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {assets} from "../assets/assets"
 const NavBar = () => {
   return (
-    <div>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
         <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button className='bg-gray-600 text-white py-2 px-5 sm:px-2 rounded-full hover:bg-gray-700'>Logout</button>
+        <button className='bg-gray-600 text-white py-2 px-5 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm hover:bg-gray-700'>Logout</button>
     </div>
   )
 }
