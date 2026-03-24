3abc3d20f184b6d45355aeec6a38b0821ddab196
NishanSunar <barailinishant908@gmail.com>
2026-03-24 22:12:47 +0545

    Admin: Sidebar designing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,38 @@
+import React from 'react'
 import NavBar from "./components/NavBar"
+import Sidebar from "./components/Sidebar"
+import {Routes, Route} from "react-router-dom"
+import Add from "./pages/Add"
+import List from "./pages/list"
+import Orders from "./pages/orders"
+import Login from "./components/Login"
 
 function App() {
-  
+  const [token, setToken] = React.useState('')
 
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+      {token === '' ? <Login />
+      :
+      <>  
       <NavBar />
-    </>
+      <hr />
+      <div className=" flex w-full">
+        <Sidebar/>
+        <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path="/add" element={<Add />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/orders" element={<Orders />} />
+
+          </Routes>
+        </div>
+      </div>
+      </>
+      }
+    </div>
   )
 }
+
 
 export default App
