1eced781e3c1f562060e56e5b2cc7ce838447e83
NishanSunar <barailinishant908@gmail.com>
2026-03-26 22:57:30 +0545

    routes and middleware for adding , updating and getting user cart data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectdb from './config/mongodb.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import connectCloudinary from './config/cloudinary.js'
+import cartRouter from './routes/cartRoute.js'
 await connectCloudinary()  // 👈 must be before app.listen
 
 // App config
@@ -24,6 +25,7 @@ app.use(cors())
 // api endpoints
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
 
 app.get('/',(req, res)=>{
     res.send("API working")
